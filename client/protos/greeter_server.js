e6b03ffc392f696168b955c6eb458a60d104a51e
Andrew Miller <jazznerd206@icloud.com>
2021-05-25 10:06:49 -0700

    basic grpc service built, make something better

---
+++ client/protos/greeter_server.js
@@ -0,0 +1,39 @@
+var PROTO_PATH = __dirname +  '/helloworld.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+/**
+ * Implements the SayHello RPC method.
+ */
+function sayHello(call, callback) {
+  callback(null, {message: 'Hello ' + call.request.name});
+}
+
+function sayHelloAgain(call, callback) {
+  callback(null, {message: 'Watch out for accidentals... 😎 ' + call.request.name});
+}
+
+/**
+ * Starts an RPC server that receives requests for the Greeter service at the
+ * sample server port
+ */
+ function main() {
+    var server = new grpc.Server();
+    server.addService(hello_proto.Greeter.service, {sayHello: sayHello, sayHelloAgain: sayHelloAgain});
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+      console.log('NSA listening on port whatever.')
+      server.start();
+    });
+  }
+  
+  main();
